2ddb8cdb65ac5f123f3cf301948bee0186290238
tomassbo <arg.tomasboo@gmail.com>
2021-12-05 13:22:48 -0300

    db-insert pokemon

--- database/migrations/2021_12_05_014831_create_pokemons_table.php
+++ database/migrations/2021_12_05_014831_create_pokemons_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreatePokemonsTable extends Migration
 {
@@ -29,6 +30,24 @@ class CreatePokemonsTable extends Migration
             $table->foreign('type_id')->references('id')->on('types');
             $table->foreign('rarity_id')->references('id')->on('rarities');
         });
+
+
+
+        $now = Carbon\Carbon::now();
+        DB::table('pokemons')->insert([
+            [
+                'name' => 'Base Set',
+                'hp' => 10,
+                'first_edition' => 1,
+                'expansion_id' => 1,
+                'type_id' => 6,
+                'rarity_id' => 1,
+                'price' => 1500,
+                'image' => 'https://cdn.shopify.com/s/files/1/0411/2623/2231/products/Charizard4-102_480x.jpg?v=1593225355',
+                'created_at' => $now->toDateTimeString()
+            ],
+
+        ]);
     }
 
     /**
